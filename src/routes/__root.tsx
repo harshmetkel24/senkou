a28fa8f3de9fffced63a4c6af7fe424d16c518dc
harshmetkel24 <harshmetkel24@gmail.com>
2025-12-03 00:41:49 +0530

    refactor: root layout changes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,13 @@
 import { TanStackDevtools } from "@tanstack/react-devtools";
 import {
   HeadContent,
+  Outlet,
   Scripts,
   createRootRouteWithContext,
   useRouterState,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
-import { NotFound } from "@/components/helpers/NotFound";
 import { FloatingHelpButton } from "../components/helpers/FloatingHelpButton";
 import { HotkeysHandlers } from "../components/helpers/HotkeysHandlers";
 import Footer from "../components/layouts/Footer";
@@ -20,7 +20,9 @@ import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
 
 import appCss from "../styles.css?url";
 
+import { NotFound } from "@/components/helpers/NotFound";
 import type { QueryClient } from "@tanstack/react-query";
+import { ReactNode } from "react";
 
 interface MyRouterContext {
   queryClient: QueryClient;
@@ -47,55 +49,65 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
       },
     ],
   }),
-
-  shellComponent: RootDocument,
+  notFoundComponent: NotFound,
+  component: RootComponent,
 });
 
 const pathsToExcludeSidebar = ["/login", "/register"];
 
-function RootDocument({ children }: { children: React.ReactNode }) {
-  const pathname = useRouterState({
-    select: (state) => state.location.pathname,
-  });
-  const sidebarVisible = pathsToExcludeSidebar.includes(pathname);
-
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en" data-theme="senkou-dark" className="dark">
       <head>
         <HeadContent />
       </head>
       <body className="relative min-h-screen  bg-background text-foreground">
-        {/* WIP: enbling this makes input in /login and /register forms unusable */}
-        {/* <StarlightBackground /> */}
-        <HotkeysHandlers />
-        {!sidebarVisible ? (
-          <>
-            <Sidebar />
-            <Header />
-            <MainContent>{children}</MainContent>
-            <Footer />
-            <FloatingHelpButton />
-          </>
-        ) : (
-          <>{children}</>
-        )}
-        {process.env.NODE_ENV === "development" && (
-          <TanStackDevtools
-            config={{
-              position: "bottom-left",
-            }}
-            plugins={[
-              {
-                name: "Tanstack Router",
-                render: <TanStackRouterDevtoolsPanel />,
-              },
-              TanStackQueryDevtools,
-            ]}
-          />
-        )}
-        <Toaster />
+        {children}
         <Scripts />
       </body>
     </html>
+  );
+}
+
+function RootComponent() {
+  const pathname = useRouterState({
+    select: (state) => state.location.pathname,
+  });
+  const sidebarVisible = pathsToExcludeSidebar.includes(pathname);
+
+  return (
+    <RootDocument>
+      {/* WIP: enbling this makes input in /login and /register forms unusable */}
+      {/* <StarlightBackground /> */}
+      <HotkeysHandlers />
+      {!sidebarVisible ? (
+        <>
+          <Sidebar />
+          <Header />
+          <MainContent>
+            <Outlet />
+          </MainContent>
+          <Footer />
+          <FloatingHelpButton />
+        </>
+      ) : (
+        <Outlet />
+      )}
+      {process.env.NODE_ENV === "development" && (
+        <TanStackDevtools
+          config={{
+            position: "bottom-left",
+          }}
+          plugins={[
+            {
+              name: "Tanstack Router",
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+            TanStackQueryDevtools,
+          ]}
+        />
+      )}
+      <Toaster />
+    </RootDocument>
   );
 }
