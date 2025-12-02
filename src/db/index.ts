4abfd761a40f2399a72e78e86d8e4d2f15edde4d
harshmetkel24 <harshmetkel24@gmail.com>
2025-12-02 15:41:19 +0530

    refactor: correct way to initiate db using neon with drizzle

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,9 @@
-import "dotenv/config";
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-export const db = drizzle(process.env.DATABASE_URL!, {
-  schema,
-});
+config({ path: ".env" });
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql, schema });
